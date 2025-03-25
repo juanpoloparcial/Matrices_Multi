50a26f0650a8ead6faa47900fce67cab6a8246e8
JuanchoStock <juanchostock@gmail.com>
2025-03-25 16:02:06 -0300

    Se agregaron detalles para mostrar por pantalla

--- multiMatriz.py
+++ multiMatriz.py
@@ -13,7 +13,7 @@ while True:
     # o también se podria agregar alguna condición para que el mismo sea un valor cercano a la realidad.
     telefonos = []
     while True:
-        tel = input ("Ingrese su num de Teléfono (para continuar presione ENTER): ")
+        tel = input ("Ingrese su num de Teléfono (Para continuar presione ENTER): ")
         #En caso de que el usuario no Ingrese ningun tel, no se mostrará ningun num
         # para eso nos sirve esta condición, la cual frena la carga de datos 
         # para los num de telefono
@@ -22,17 +22,17 @@ while True:
         telefonos.append(tel) #En caso de que si haya numero, se agrega a los datos
         #de la persona. 
         
-    personas.append([nombre,apellido, dni,"Teléfonos: ", telefonos])
+    personas.append([nombre,apellido,"DNI: ", dni,"Teléfonos: ", telefonos])
 
     #Se le consulta al usuario si desea seguir agregando más personas a la lista
-    agregar_persona = input("Quiere Agregar otra persona ?  (SI/NO): ").upper()
+    agregar_persona = input('\n'"¿ Quiere Agregar otra persona ?  (SI/NO): ").upper()
     if agregar_persona != "SI":
         break
     #Cualquier respuesta que el usuario ingrese que no se "SI", se tomara como un "NO"
     # y se finalizara la carga de otra persona a la lista. En caso de que responda de forma
     # correcta, se habilita a que pueda ingresar una nueva persona a la lista.
     
-print('\n'"Lista de Personas: ")
+print('\n'" - - Lista de Personas - -" '\n')
 for i in personas:
     print(i)
     #Realizamos un ciclo For para que muestre por pantalla 
